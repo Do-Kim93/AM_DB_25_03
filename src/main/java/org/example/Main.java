938ad5fc46eaf05bed44c97bca6237ac94f16b3d
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-28 09:20:51 +0900

    커밋 테스트

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("커밋텍스트");
+    }
+}
