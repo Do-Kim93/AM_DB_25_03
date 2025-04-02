d314d2bf598212b96510339161d7bb5700060398
Do-Kim93 <ehhgus200293@gmail.com>
2025-04-02 09:50:20 +0900

    회원가입 완성 내V

--- DB.sql
+++ DB.sql
@@ -9,6 +9,14 @@ CREATE TABLE article(
                         title CHAR(100),
                         `body` TEXT NOT NULL
 );
+CREATE TABLE member(
+                        id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        regDate DATETIME NOT NULL,
+                        updateDate DATETIME NOT NULL,
+                        loginId CHAR(100) NOT NULL unique,
+                        loginPw TEXT NOT NULL,
+                        name CHAR(100) NOT NULL
+);
 insert INTO article SET regDate = NOW(),updateDate = NOW(),title = '제목', `body` = '내용';
 
 create table article(
@@ -32,3 +40,29 @@ select count (*) from article where id = 19;
 # auto_increment 초기화 하는 코드
 SET @count=0;
 UPDATE article SET id =@count:=@count+1;
+/*
+# 1. 손흥민의 주문 개수는? ???
+SELECT COUNT(*) FROM t_shopping WHERE userName = '손흥민';
+SELECT COUNT(userName) FROM t_shopping;
+# 2. 손흥민이 산 상품은? ???
+SELECT pname FROM t_shopping WHERE userName = '손흥민';
+# 3. 스커트를 산 사람은? ???
+SELECT DISTINCT userName FROM t_shopping WHERE pname = '스커트';
+# 4. 가장 많이 주문한 사람의 아이디와 이름, 주문개수는? ???
+SELECT * FROM t_shopping WHERE MAX(price);
+SELECT userName, userId, COUNT(*) AS 'order' FROM t_shopping  GROUP BY userId ORDER BY `order` DESC LIMIT 1;
+# 5. 소지섭이 사용한 총 금액은? ???
+SELECT SUM(price) FROM t_shopping WHERE userName = '소지섭';
+*/
+/*
+ # 1. 손흥민의 주문 개수는? ???
+SELECT COUNT(userNo) AS '주문개수' FROM t_order INNER JOIN t_user ON t_order.userNo = t_user.id WHERE t_user.userName = '손흥민';
+# 2. 손흥민이 산 상품은? ??? id1
+SELECT DISTINCT t_product.pname FROM t_product INNER JOIN t_order ON t_product.id = t_order.productNo INNER JOIN t_user ON t_order.userNo = t_user.id WHERE t_user.userName = '손흥민';
+# 3. 스커트를 산 사람은? ???
+SELECT DISTINCT t_user.userName AS '고객이름' FROM t_user INNER JOIN t_order ON t_user.id = t_order.userNo INNER JOIN t_product ON t_product.id = t_order.productNo WHERE t_product.pname = '스커트';
+# 4. 가장 많이 주문한 사람의 아이디와 이름, 주문개수는? ???
+SELECT t_user.userId AS '고객아이디', t_user.userName AS '고객이름', COUNT(t_order.userNo) AS '주문수량' FROM t_order INNER JOIN t_user ON t_order.userNo = t_user.id GROUP BY t_user.userName ORDER BY 주문수량 DESC LIMIT 1;
+# 5. 소지섭이 사용한 총 금액은? ???
+SELECT DISTINCT SUM(t_product.price) AS '사용금액' FROM t_user INNER JOIN t_order ON t_user.id = t_order.userNo INNER JOIN t_product ON t_product.id = t_order.productNo WHERE t_user.userName = '소지섭';
+ */
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,10 @@ package org.example;
 import org.example.util.DBUtil;
 import org.example.util.SecSql;
 
-import java.sql.*;
+import java.lang.reflect.Member;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -88,7 +91,7 @@ public class App {
             SecSql sqlall = new SecSql();
             sqlall.append("SELECT count(*) FROM article");
             int count = DBUtil.selectRowIntValue(conn, sqlall);
-            System.out.println(count+ "개의 글이 있습니다.");
+            System.out.println(count + "개의 글이 있습니다.");
 
             SecSql sql = new SecSql();
             sql.append("SELECT *");
@@ -214,6 +217,74 @@ public class App {
             DBUtil.delete(conn, sql);
 
             System.out.println(id + "번 글이 삭제되었습니다.");
+        } else if (cmd.equals("member join")) {
+            System.out.println("==회원가입==");
+            String loginid;
+            while (true) {
+                System.out.print("아이디 : ");
+                loginid = sc.nextLine().trim();
+                SecSql sql = new SecSql();
+                sql.append("SELECT *");
+                sql.append("FROM member");
+                sql.append("WHERE loginId = ?;", loginid);
+                Map<String, Object> memberMap = DBUtil.selectRow(conn, sql);
+                if (memberMap.isEmpty()) {
+                    System.out.println("사용 가능 아이디");
+                    break;
+                } else {
+                    System.out.println("중복 아디임");
+                }
+            }
+            while (true){
+                System.out.print("비밀번호 : ");
+                String pw = sc.nextLine().trim();
+                System.out.print("비밀번호 확인 : ");
+                String pw1 = sc.nextLine().trim();
+                if (pw.equals(pw1)){
+                    SecSql sql = new SecSql();
+                    System.out.print("이름 : ");
+                    String name = sc.nextLine().trim();
+                    sql.append("INSERT INTO member");
+                    sql.append("SET regDate = NOW(),");
+                    sql.append("updateDate = NOW(),");
+                    sql.append("loginId = ?,", loginid);
+                    sql.append("loginPw = ?,", pw);
+                    sql.append("name = ?;", name);
+                    int id1 = DBUtil.insert(conn, sql);
+                    System.out.println(id1 + "번째 회원 가입 입니다.");
+                    break;
+                }else System.out.println("비번 다시 입력해");
+            }
+
+        }else if (cmd.equals("member list")) {
+            System.out.println("==회원목록==");
+            List<Member> members = new ArrayList<>();
+
+            SecSql sqlall = new SecSql();
+            sqlall.append("SELECT count(*) FROM article");
+            int count = DBUtil.selectRowIntValue(conn, sqlall);
+            System.out.println(count + "개의 글이 있습니다.");
+
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("ORDER BY id DESC");
+
+            List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
+
+            for (Map<String, Object> articleMap : articleListMap) {
+                members.add(new Article(articleMap));
+            }
+
+            if (articles.size() == 0) {
+                System.out.println("게시글이 없습니다");
+                return 0;
+            }
+
+            System.out.println("  번호  /   제목  ");
+            for (Article article : articles) {
+                System.out.printf("  %d     /   %s   \n", article.getId(), article.getTitle());
+            }
         }
 
 
